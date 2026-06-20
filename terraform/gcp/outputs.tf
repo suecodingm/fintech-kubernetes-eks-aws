907b63727deaf65b08f1843bd4d9152e220d6c01
suecodingm <susana.mendoza.neira@gmail.com>
2026-06-20 18:35:27 -0500

    Delete terraform directory

--- terraform/gcp/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "control_plane_ip" {
-  value = google_compute_instance.control_plane.network_interface[0].access_config[0].nat_ip
-}
-
-output "worker_ip" {
-  value = google_compute_instance.worker.network_interface[0].access_config[0].nat_ip
-}
